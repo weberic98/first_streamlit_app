4039a6659f6d3e88efd5475ae85044c08bb9dec4
weberic98 <156134452+weberic98@users.noreply.github.com>
2024-02-21 16:41:30 -0600

    Update streamlit_app.py
    remove raw json display
    separate base url from variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # normalize json for readability 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display normalized json
+# display normalized json as a table
 streamlit.dataframe(fruityvice_normalized)
